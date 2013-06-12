9e2f68ffe765d544668a9e78a912a30f79ed8706
Miguel Mawyin <mawyin.miguel@gmail.com>
2013-06-12 00:25:27 -0700

    Comments

--- FTP/FTPBundle.php
+++ FTP/FTPBundle.php
@@ -1,4 +1,9 @@
 <?php
+	/**
+	* Author : Miguel Mawyin
+	* Last Modified : June 11, 2013
+	* Description : Renegade's FTP bundle
+	*/
 	namespace Renegade\FTP;
 	use Symfony\Component\HttpKernel\Bundle\Bundle;
 	
