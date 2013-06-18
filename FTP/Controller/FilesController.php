db9dc5f67a3c4c570b3ccf6ca3cbc01e73c6bfc0
Miguel Mawyin <mawyin.miguel@gmail.com>
2013-06-17 21:11:12 -0700

    Started on the file upload/download

---
+++ FTP/Controller/FilesController.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	* Author : Miguel Mawyin
+	* Date Created : June 17, 2013
+	* Description :
+	*/
+	namespace Renegade\FTP\Controller;
+	
+	use Renegade\FTP\Entity\User;
+	use Renegade\FTP\Entity\Database;
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\HttpFoundation\Session\Session;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	
+	class FilesController extends Controller
+	{
+		public function uploadAction(){
+			
+		}
+	}
---
+++ FTP/Entity/File.php
@@ -0,0 +1,10 @@
+<?php
+	namespace Renegade\FTP\Entity;
+	
+	use Doctrine\ORM\Mapping as ORM;
+	use Symfony\Component\Validator\Constraints as Assert;
+	
+	class File
+	{
+		
+	}
